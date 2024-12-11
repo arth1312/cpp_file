604953a50f820e423b54d4ccaa59ab214fca4778
arth1312 <arthkoradiya@gmail.com>
2024-12-11 18:14:02 +0530

    01_ if_else 05/12/2024

---
+++ if-else.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a, b; 
+    cout << "Enter value a, and b:";
+    cin >> a >> b;
+    if (a > b)
+    {
+        cout << "a is max..";
+    }
+    else
+    {
+        cout << "b is max..";
+    }
+}
