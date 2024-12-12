cdb6f857013216081e087520ad7b54039f0f5106
arth1312 <arthkoradiya@gmail.com>
2024-12-12 18:13:59 +0530

    loop 12/12/2024

---
+++ loop.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    /* 
+    while (i <= 10)
+    {
+        cout << i << endl;
+        i++;
+    } 
+    */
+
+   /*
+   do
+    {
+        cout << i << endl;
+        i++;
+    } while (i <= 10); 
+    */
+
+   for (i; i <= 10; i++)
+   {
+    cout << i << endl;
+   }
+   
+    return 0;
+}
