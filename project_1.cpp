7590147439e61af798ff7b95a6a05f5b413f0d09
arth1312 <arthkoradiya@gmail.com>
2024-12-14 18:11:10 +0530

    project_1 14/12/2024

---
+++ project_1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int score;
+    char grade;
+    cout << "Enter your score: ";
+    cin >> score;
+    (score >= 90)
+        ? cout << "A" :
+        (score >= 80)
+        ? cout << "B" :
+        (score >= 70)
+        ? cout << "C" :
+        (score >= 60)
+        ? cout << "D" : cout << "F";
+    switch ()
+    {
+    case 
+        break;
+    
+    default:
+        break;
+    }
+    return 0;
+}
